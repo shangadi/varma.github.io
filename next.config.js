1a083d9aef294761071eeaa61da514771e411fce
shangadi <25998394+shangadi@users.noreply.github.com>
2024-10-25 02:53:44 +0000

    initial commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next/config').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    basePath: '/varma.github.io', // Replace with your repository name
+  }
+  
+  module.exports = nextConfig
